e761c8dfaab7a31f7222b6b07d59cfc9c439ec61
Rajdeep Singha <68198116+Suzie97@users.noreply.github.com>
2021-09-27 15:46:06 +0530

    Delete libraries directory

--- libraries/htmlclean/htmlclean.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/**
- * Strips HTML from the input string and returns just the text.
- * The resulting string must be freed when you're done with it.
- */
-char *htmlclean_strip_html(const char *);
